2722c71fcd05f6150762794e7b17b608fb12aa2d
Ryan Chung <water6494@hotmail.com.hk>
2025-08-17 02:45:05 +0800

    feat: sheet picker

--- src/pages/SheetConfigPage/SheetConfigPage.tsx
+++ src/pages/SheetConfigPage/SheetConfigPage.tsx
@@ -1,11 +1,13 @@
 import { Button, FormInput, Title } from "@components";
 import { useAuth } from "@hooks/useAuth";
 import { useCanGoBack } from "@hooks/useCanGoBack";
-import { useSheetStore } from "@stores";
+import { useAuthStore, useSheetStore } from "@stores";
 import { isNil } from "lodash";
 import { useCallback, useState } from "react";
 import { useNavigate } from "react-router";
 import { path } from "src/routes/constants/path";
+import useDrivePicker from "react-google-drive-picker";
+import type { PickerConfiguration } from "react-google-drive-picker/dist/typeDefs";
 
 export const SheetConfigPage = () => {
   const { sheetId, sheetIndex, setSheetId, setSheetIndex } = useSheetStore();
@@ -17,6 +19,7 @@ export const SheetConfigPage = () => {
   const navigate = useNavigate();
   const canGoBack = useCanGoBack();
   const { isAuth } = useAuth();
+  const { token } = useAuthStore();
   const isConfiguredBefore = !!sheetId && !isNil(sheetIndex);
   const isGoBackShown = isAuth && isConfiguredBefore;
 
@@ -52,17 +55,44 @@ export const SheetConfigPage = () => {
     }
   }, [canGoBack, navigate]);
 
+  const [openPicker] = useDrivePicker();
+
+  const onSelectDoc = useCallback(() => {
+    const config: PickerConfiguration = {
+      clientId: import.meta.env.VITE_GOOGLE_OAUTH_CLIENT_ID,
+      developerKey: import.meta.env.VITE_GOOGLE_API_KEY,
+      viewId: "SPREADSHEETS",
+      token,
+      showUploadView: true,
+      showUploadFolders: true,
+      supportDrives: true,
+      callbackFunction: ({ action, docs }) => {
+        if (action === "cancel" || docs.length === 0) {
+          return;
+        }
+        const sheetId = docs[0].id;
+        _setSheetId(sheetId);
+      },
+    };
+    openPicker(config);
+  }, [openPicker, token]);
+
   return (
     <div className="p-8 flex flex-col gap-10">
       <Title>Sheet Parameters</Title>
       <div className="flex flex-col gap-6">
-        <FormInput
-          id="sheetId"
-          label="Spreadsheet Id"
-          required
-          value={_sheetId}
-          onChange={(e) => _setSheetId(e.target.value)}
-        />
+        <div className="flex gap-4 items-center">
+          <FormInput
+            id="sheetId"
+            label="Spreadsheet Id"
+            required
+            value={_sheetId}
+            readOnly
+            onChange={(e) => _setSheetId(e.target.value)}
+            className="flex-1"
+          />
+          <Button onClick={onSelectDoc}>Select</Button>
+        </div>
         <FormInput
           id="sheetId"
           label="Main sheet Index"
