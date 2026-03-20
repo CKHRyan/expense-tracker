a15d6aa5754b85455e55058334957ac345441188
Ryan Chung <water6494@hotmail.com.hk>
2026-03-20 21:54:27 +0800

    Update env config

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -2,8 +2,6 @@ const env = import.meta.env;
 
 export const config = {
   googleApiKey: env.VITE_GOOGLE_API_KEY,
-  googleServiceAccountApiKey: env.VITE_GOOGLE_SERVICE_ACCOUNT_API_KEY,
-  pocSheetId: env.VITE_POC_SHEET_ID,
   googleOAuthClientId: env.VITE_GOOGLE_OAUTH_CLIENT_ID,
   authServiceHost: env.VITE_AUTH_SERVICE_HOST,
   enableAuthService: env.VITE_ENABLE_AUTH_SERVICE === "true",
